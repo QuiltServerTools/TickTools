2ebf261093dbc3db6337d40e00a397b6bfd8a868
solonovamax <solonovamax@12oclockpoint.com>
2021-12-10 16:01:42 -0500

    Update to 1.18

--- src/main/java/com/github/quiltservertools/ticktools/mixin/MixinServerPlayerEntity.java
+++ src/main/java/com/github/quiltservertools/ticktools/mixin/MixinServerPlayerEntity.java
@@ -1,21 +1,29 @@
 package com.github.quiltservertools.ticktools.mixin;
 
-import com.github.quiltservertools.ticktools.TickToolsManager;
+
 import net.minecraft.network.packet.s2c.play.ChunkLoadDistanceS2CPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerPlayerEntity.class)
-public class MixinServerPlayerEntity {
 
+@Mixin(ServerPlayerEntity.class)
+public abstract class MixinServerPlayerEntity {
+    @Shadow
+    public ServerPlayNetworkHandler networkHandler;
+    
     @Inject(method = "worldChanged", at = @At("HEAD"))
     public void ticktools$syncRenderDistance(ServerWorld origin, CallbackInfo ci) {
-        ServerWorld world = ((ServerPlayerEntity) (Object) this).getServerWorld();
+        ServerWorld world = this.getWorld();
         int distance = ((MixinThreadedAnvilChunkStorage) world.getChunkManager().threadedAnvilChunkStorage).getWatchDistance();
-        ((ServerPlayerEntity) (Object) this).networkHandler.sendPacket(new ChunkLoadDistanceS2CPacket(distance));
+        networkHandler.sendPacket(new ChunkLoadDistanceS2CPacket(distance));
     }
+    
+    @Shadow
+    public abstract ServerWorld getWorld();
 }
